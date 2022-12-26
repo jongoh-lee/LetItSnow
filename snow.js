87b6aa4bf5bd97102292834afd20d2af7efa9c26
jongoh-lee <sky40165285@gmail.com>
2022-12-26 14:43:31 +0900

    Feat: 눈이 내리는 화면

---
+++ snow.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body");
+const MIN_DURATION = 10;
+function letItSnow() {
+  const div = document.createElement("div");
+  div.classList.add("snowflake");
+  const delay = Math.random() * 10;
+  const initialOpacity = Math.random();
+  const duration = Math.random() * 20 + MIN_DURATION;
+
+  div.style.left = `${Math.random() * window.screen.width}px`;
+  div.style.animationDelay = delay + "s";
+  div.style.opacity = initialOpacity;
+  div.style.animationDuration = duration + "s";
+
+  body.appendChild(div);
+}
+
+for (let i = 0; i < 50; i++) {
+  letItSnow();
+}
